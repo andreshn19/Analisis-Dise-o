1f7d9ca69398efde80370faf5888de1571d4338d
m3rariunah <fmventura@unah.hn>
2022-03-12 22:25:02 -0600

    Login

---
+++ JS/Login.js
@@ -0,0 +1,10 @@
+function go(){
+
+    if (document.form.password.value=='password' && document.form.login.value=='username'){ 
+            document.form.submit(); 
+        } 
+        else{ 
+             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+        } 
+    } 
+    
