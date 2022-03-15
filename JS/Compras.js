f33df11818b9ab1edeb1f0d060dd82bcf348ef52
Jonathan-Caballero <jicaballero@unah.hn>
2022-03-14 18:28:37 -0600

    Avance modulo compras

--- JS/Compras.js
+++ JS/Compras.js
@@ -8,3 +8,55 @@ event.preventDefault();
         $(this).children('i:last-child').toggleClass('fa-caret-down fa-caret-left');
     }
 });
+
+
+var app = angular.module("listaCompras", []);
+app.controller("listaComprasController", function($scope, $filter) {
+  $scope.itens = [{
+  }];
+  
+  $scope.adicionaItem = function() {
+    $scope.itens.push({
+      descricao: $scope.item.descricao,
+      valor: $scope.item.valor,
+      data: $scope.item.data
+    });
+    $scope.item.descricao = $scope.item.valor = $scope.item.data = '';
+    $scope.valorTotal = $scope.sum($scope.itens, 'valor'); 
+  };
+  
+  $scope.sum = function(items, prop){
+      return items.reduce( function(a, b){
+          return a + b[prop];
+      }, 0);
+  };
+  $scope.valorTotal = $scope.sum($scope.itens, 'valor'); 
+  
+  $scope.submitForm = function() {
+
+			// check to make sure the form is completely valid
+			if ($scope.formItem.$valid) {
+				console.log($scope.formItem.data);
+        console.log($scope.formItem.valor);
+        console.log($scope.formItem.descricao);
+        $scope.adicionaItem();
+			}else{
+        console.log("form invalid");
+      }
+
+		};
+  
+});
+
+app.filter('sumOfValue', function () {
+    return function (data, key) {
+        if (typeof (data) === 'undefined' && typeof (key) === 'undefined') {
+            return 0;
+        }
+        var sum = 0;
+        for (var i = 0; i < data.length; i++) {
+            sum = sum + parseInt(data[i][key]);
+        }
+        return sum;
+    }
+})
