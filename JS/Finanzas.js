58969f24850df4f446780227e412bedb3b24dafe
Esperanza17 <esperanza.cartagena@unah.hn>
2022-03-22 22:58:52 -0600

    Mejoras Finanzas

---
+++ JS/Finanzas.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+	//Only needed for the filename of export files.
+	//Normally set in the title tag of your page.
+	document.title = "Reportes";
+	// Create search inputs in footer
+	$("#example tfoot th").each(function () {
+		var title = $(this).text();
+		$(this).html('<input type="text" placeholder="Search ' + title + '" />');
+	});
+	// DataTable initialisation
+	var table = $("#example").DataTable({
+		dom: '<"dt-buttons"Bf><"clear">lirtp',
+		paging: true,
+		autoWidth: true,
+		buttons: [
+			"colvis",
+			"copyHtml5",
+			"csvHtml5",
+			"excelHtml5",
+			"print"
+		],
+		initComplete: function (settings, json) {
+			var footer = $("#example tfoot tr");
+			$("#example thead").append(footer);
+		}
+	});
+
+	// Apply the search
+	$("#example thead").on("keyup", "input", function () {
+		table.column($(this).parent().index())
+		.search(this.value)
+		.draw();
+	});
+});
